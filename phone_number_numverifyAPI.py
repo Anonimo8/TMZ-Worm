6d368b0531e2ccc28a3d45cf322274f5be4674d4
Anonimo8 <61224709+Anonimo8@users.noreply.github.com>
2021-05-14 16:22:47 -0300

    Update phone_number_numverifyAPI.py

--- phone_number_numverifyAPI.py
+++ phone_number_numverifyAPI.py
@@ -2,14 +2,20 @@ import os
 
 num = int(input("Numero de telefono: "))
 
+#Comentar este comando en caso de tener instalado curl
 #os.system('apt-get install curl')
 
 os.system('clear')
 
-os.system(f'curl "http://apilayer.net/api/validate?access_key=9bbe084951ab3183978a8ade88f90532&number={num}&country_code=&format=1" ')
+os.system(f'curl "http://apilayer.net/api/validate?access_key=9bbe084951ab3183978a8ade88f90532&number={num}&country_code=&format=1" | sed -e "s/^.//" -e "s/.$//" > {num}.txt')
+os.system(f'chmod 777 {num}.txt')
+os.system('clear')
+os.system(f'cat -T {num}.txt')
+
 
 print('\n\n')
 input('Preisona una tecla para continuar...')
+
 os.system('clear')
 
-#curl -H "" | json_ppasd
+
